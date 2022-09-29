b2697e8d1c10dc64b7d72c20fde22578a3cb10f7
mdsajib50 <mdajib850@gmail.com>
2022-09-29 09:10:09 +0600

    header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className ='fs-1 fw-bold text-info m-5'>
+            <h1>GYM-Club-BD</h1>
+        </div>
+    );
+};
+
+export default Header;
