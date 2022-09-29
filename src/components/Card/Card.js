329f169137cfd859738e03322c0c21a7b5cf08c3
mdsajib50 <mdajib850@gmail.com>
2022-09-29 11:00:59 +0600

    card component added

---
+++ src/components/Card/Card.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="row">
+        <div className="col-sm-4">
+            <div className="card">
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div className="col-sm-4">
+            <div className="card">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div className="col-sm-4">
+            <div className="card">
+            <img src="..." class="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Card;
